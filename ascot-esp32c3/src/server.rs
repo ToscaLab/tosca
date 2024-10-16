58f88f8faf2d3ffb7311357e5a1667572346af48
Luni-4 <luni-4@hotmail.it>
2024-10-16 11:42:47 +0200

    esp32-c3: Remove dead code

--- ascot-esp32c3/src/server.rs
+++ ascot-esp32c3/src/server.rs
@@ -15,57 +15,6 @@ const DEFAULT_SERVER_PORT: u16 = 3000;
 // Server stack size.
 const DEFAULT_STACK_SIZE: usize = 10240;
 
-/*// Define a macro to reduce the binary size dimension and reuse the server code.
-macro_rules! server {
-    ($self: ident) => {
-        let mut server = EspHttpServer::new(&Configuration {
-            stack_size: $self.stack_size,
-            http_port: $self.port,
-            ..Default::default()
-        })?;
-
-        // Format the device description as a pretty string.
-        let device_description = serde_json::to_string_pretty(&$self.device.serialize_data())?;
-
-        for route in $self.device.routes_data {
-            let method = match route.route_hazards.route.kind() {
-                RestKind::Get => Method::Get,
-                RestKind::Post => Method::Post,
-                RestKind::Put => Method::Put,
-                RestKind::Delete => Method::Delete,
-            };
-            if let Some(body) = route.body {
-                server.fn_handler(
-                    &format!(
-                        "{}{}",
-                        $self.device.main_route,
-                        route.route_hazards.route.route()
-                    ),
-                    method,
-                    move |req| {
-                        // Run body.
-                        body()?;
-
-                        // Write response.
-                        (route.response)(req)?.write_all(route.content.as_bytes())
-                    },
-                )?;
-            } else {
-                server.fn_handler(route.route_hazards.route.route(), method, move |req| {
-                    // Write only response.
-                    (route.response)(req)?.write_all(route.content.as_bytes())
-                })?;
-            }
-        }
-
-        // Add main route
-        server.fn_handler($self.device.main_route, Method::Get, move |req| {
-            req.into_response(200, Some("OK"), &[("Content-Type", "application/json")])?
-                .write_all(device_description.as_bytes())
-        })?;
-    };
-}*/
-
 /// The `Ascot` server.
 pub struct AscotServer {
     // Server port.
@@ -106,15 +55,6 @@ impl AscotServer {
         self.service_config = Some(service_config);
         self
     }
-
-    /*/// Runs a device on a server with a service.
-    pub fn run_with_service(self, service_builder: ServiceBuilder) -> Result<()> {
-        // Run server
-        server!(self);
-
-        // Run service
-        InternalService::run(service_builder)
-    }*/
 
     /// Runs the server.
     pub fn run(self) -> Result<()> {
