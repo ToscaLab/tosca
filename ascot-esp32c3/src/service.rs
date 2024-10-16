23f7b31cf22da87fd4ba28769f1ce490f35d44dd
Luni-4 <luni-4@hotmail.it>
2024-10-16 10:27:16 +0200

    esp32-c3: Refactor service

--- ascot-esp32c3/src/service.rs
+++ ascot-esp32c3/src/service.rs
@@ -26,131 +26,136 @@ const SERVICE_HOSTNAME: &str = "ascot";
 // Service name
 const SERVICE_NAME: &str = "ascot";
 
-pub(crate) mod internal_service {
-    pub trait Service: Sized {
-        fn run(self) -> super::Result<()>;
-    }
+#[repr(u8)]
+enum Algorithm {
+    MdnsSd,
 }
 
 /// A mDNS-SD service.
-pub struct MdnsSdService {
+pub struct ServiceBuilder {
+    algorithm: Algorithm,
     http_address: Ipv4Addr,
-    service_hostname: &'static str,
-    service_name: &'static str,
-    stack: Stack,
-    send_buf: VecBufAccess<NoopRawMutex, 1500>,
-    recv_buf: VecBufAccess<NoopRawMutex, 1500>,
+    hostname: &'static str,
+    domain_name: &'static str,
 }
 
-impl internal_service::Service for MdnsSdService {
-    #[inline(always)]
-    fn run(self) -> Result<()> {
-        block_on(self.mdns_sd_service())
-    }
-}
-
-impl MdnsSdService {
-    /// Creates a new [`MdnsSdService`] instance.
-    pub fn new(http_address: Ipv4Addr) -> Self {
-        // Create stack
-        let stack = Stack::new();
-
-        // Create sender and receiver buffers for mdns-sd protocol.
-        let (recv_buf, send_buf) = (VecBufAccess::new(), VecBufAccess::new());
-
+impl ServiceBuilder {
+    /// Creates a new [`ServiceBuilder`] instance for `mDNS-SD`.
+    pub const fn mdns_sd(http_address: Ipv4Addr) -> Self {
         Self {
+            algorithm: Algorithm::MdnsSd,
             http_address,
-            service_hostname: SERVICE_HOSTNAME,
-            service_name: SERVICE_NAME,
-            stack,
-            send_buf,
-            recv_buf,
+            hostname: SERVICE_HOSTNAME,
+            domain_name: SERVICE_NAME,
         }
     }
 
-    /// Sets a service hostname.
-    pub const fn hostname(mut self, service_hostname: &'static str) -> Self {
-        self.service_hostname = service_hostname;
+    /// Sets a service host name.
+    pub const fn hostname(mut self, hostname: &'static str) -> Self {
+        self.hostname = hostname;
         self
     }
 
-    /// Sets a service name.
-    pub const fn name(mut self, service_name: &'static str) -> Self {
-        self.service_name = service_name;
+    /// Sets a service domain name.
+    pub const fn domain_name(mut self, domain_name: &'static str) -> Self {
+        self.domain_name = domain_name;
         self
     }
+}
 
-    async fn mdns_sd_service(self) -> Result<()> {
+async fn mdns_sd_service(
+    hostname: &'static str,
+    domain_name: &'static str,
+    http_address: Ipv4Addr,
+) -> Result<()> {
+    // Create stack
+    let stack = Stack::new();
+
+    // Create sender and receiver buffers for mdns-sd protocol.
+    let (recv_buf, send_buf): (
+        VecBufAccess<NoopRawMutex, 1500>,
+        VecBufAccess<NoopRawMutex, 1500>,
+    ) = (VecBufAccess::new(), VecBufAccess::new());
+
+    // No ipv6 up and running.
+    // To have it running, we need to get at least a link-local ipv6 addr
+    // first, using an `esp-idf-sys` API call once the wifi is up and running:
+    // `esp_idf_svc::sys::esp_netif_create_ip6_linklocal`.
+    // Moreover, we can't just pass "0" for the interface.
+    // We need to pass `wifi.sta_netif().index()`
+    // Sometimes, "0" does work on PCs, but not with ESP-IDF.
+    // This API is very picky about having a correct ipv6-capable
+    // interface rather than just "all" (= 0).
+    let mut socket = io::bind(&stack, DEFAULT_SOCKET, Some(Ipv4Addr::UNSPECIFIED), None).await?;
+
+    let (recv, send) = socket.split();
+
+    info!(
+        "About to run an mDNS responder reachable from a PC. \
+             It will be addressable using {hostname}.local, \
+             so try to `ping {hostname}.local`."
+    );
+
+    let host = Host {
+        hostname,
+        ipv4: http_address,
+        ipv6: Ipv6Addr::UNSPECIFIED,
+        ttl: Ttl::from_secs(60),
+    };
+
+    info!(
+        "About to run an mDNS service with name `{domain_name}` of type `HTTPS` \
+             on port `443`."
+    );
+
+    let service = Service {
+        name: domain_name,
+        priority: 1,
+        weight: 5,
+        service: "_https",
+        protocol: "_tcp",
+        port: 443,
+        service_subtypes: &[],
+        txt_kvs: &[],
+    };
+
+    // A way to notify the mDNS responder that the data in `Host` had changed
+    // Not necessary for this example, because the data is hard-coded
+    let signal = Signal::new();
+
+    let mdns = io::Mdns::<NoopRawMutex, _, _, _, _>::new(
+        Some(Ipv4Addr::UNSPECIFIED),
         // No ipv6 up and running.
-        // To have it running, we need to get at least a link-local ipv6 addr
-        // first, using an `esp-idf-sys` API call once the wifi is up and running:
-        // `esp_idf_svc::sys::esp_netif_create_ip6_linklocal`.
-        // Moreover, we can't just pass "0" for the interface.
-        // We need to pass `wifi.sta_netif().index()`
-        // Sometimes, "0" does work on PCs, but not with ESP-IDF.
-        // This API is very picky about having a correct ipv6-capable
-        // interface rather than just "all" (= 0).
-        let mut socket = io::bind(
-            &self.stack,
-            DEFAULT_SOCKET,
-            Some(Ipv4Addr::UNSPECIFIED),
-            None,
-        )
-        .await?;
+        None,
+        recv,
+        send,
+        recv_buf,
+        send_buf,
+        |buf| thread_rng().fill_bytes(buf),
+        &signal,
+    );
 
-        let (recv, send) = socket.split();
+    mdns.run(HostAnswersMdnsHandler::new(ServiceAnswers::new(
+        &host, &service,
+    )))
+    .await
+    .map_err(|e| e.into())
+}
 
-        info!(
-            "About to run an mDNS responder reachable from a PC. \
-             It will be addressable using {}.local, \
-             so try to `ping {}.local`.",
-            self.service_hostname, self.service_hostname
-        );
+pub(crate) struct InternalService;
 
-        let host = Host {
-            hostname: self.service_hostname,
-            ipv4: self.http_address,
-            ipv6: Ipv6Addr::UNSPECIFIED,
-            ttl: Ttl::from_secs(60),
-        };
+impl InternalService {
+    #[inline(always)]
+    pub(crate) fn run(service_builder: ServiceBuilder) -> Result<()> {
+        let ServiceBuilder {
+            algorithm,
+            http_address,
+            hostname,
+            domain_name,
+        } = service_builder;
 
-        info!(
-            "About to run an mDNS service with name `{}` of type `HTTPS` \
-             on port `443`.",
-            self.service_name
-        );
-
-        let service = Service {
-            name: self.service_name,
-            priority: 1,
-            weight: 5,
-            service: "_https",
-            protocol: "_tcp",
-            port: 443,
-            service_subtypes: &[],
-            txt_kvs: &[],
-        };
-
-        // A way to notify the mDNS responder that the data in `Host` had changed
-        // Not necessary for this example, because the data is hard-coded
-        let signal = Signal::new();
-
-        let mdns = io::Mdns::<NoopRawMutex, _, _, _, _>::new(
-            Some(Ipv4Addr::UNSPECIFIED),
-            // No ipv6 up and running.
-            None,
-            recv,
-            send,
-            self.recv_buf,
-            self.send_buf,
-            |buf| thread_rng().fill_bytes(buf),
-            &signal,
-        );
-
-        mdns.run(HostAnswersMdnsHandler::new(ServiceAnswers::new(
-            &host, &service,
-        )))
-        .await
-        .map_err(|e| e.into())
+        block_on(match algorithm {
+            Algorithm::MdnsSd => mdns_sd_service(hostname, domain_name, http_address),
+        })
     }
 }
