3a4467000cd092aff0222ce47783156b6ed0173c
Luni-4 <luni-4@hotmail.it>
2024-10-30 13:15:49 +0100

    axum: Define a general device with new device actions

--- ascot-axum/src/device.rs
+++ ascot-axum/src/device.rs
@@ -5,25 +5,33 @@ use axum::Router;
 
 use tracing::info;
 
-use crate::actions::Action;
+use crate::actions::DeviceAction;
 
 // Default main route for a device.
 const DEFAULT_MAIN_ROUTE: &str = "/device";
 
-/// A general smart home device.
+/// A general device.
 #[derive(Debug)]
-pub struct Device {
+pub struct Device<S = ()>
+where
+    S: Clone + Send + Sync + 'static,
+{
     // Main device route.
     pub(crate) main_route: &'static str,
     // Router.
     pub(crate) router: Router,
+    // State.
+    pub(crate) state: S,
     // Kind.
     kind: DeviceKind,
     // All device routes and their hazards.
     routes_hazards: RoutesHazards,
 }
 
-impl DeviceSerializer for Device {
+impl<S> DeviceSerializer for Device<S>
+where
+    S: Clone + Send + Sync + 'static,
+{
     fn serialize_data(&self) -> DeviceData {
         let mut route_configs = RouteConfigs::empty();
         for route_hazards in self.routes_hazards.iter() {
@@ -45,11 +53,28 @@ impl DeviceSerializer for Device {
     }
 }
 
-impl Device {
-    /// Creates an unknown [`Device`].
+impl Default for Device<()> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Device<()> {
+    /// Creates an unknown [`Device`] without a state.
+    #[inline(always)]
+    pub fn new() -> Self {
+        Self::with_state(())
+    }
+}
+
+impl<S> Device<S>
+where
+    S: Clone + Send + Sync + 'static,
+{
+    /// Creates an unknown [`Device`] with state.
     #[inline]
-    pub fn unknown() -> Self {
-        Self::new(DeviceKind::Unknown)
+    pub fn with_state(state: S) -> Self {
+        Self::init(DeviceKind::Unknown, state)
     }
 
     /// Sets a new main route.
@@ -58,24 +83,29 @@ impl Device {
         self
     }
 
-    /// Adds an [`Action`] to the device.
+    /// Adds an action to the [`Device`].
     #[inline]
-    pub fn add_action(mut self, device_chainer: impl Action) -> Self {
-        let (router, route_hazards) = device_chainer.data();
-
-        self.router = self.router.merge(router);
-        self.routes_hazards.add(route_hazards);
-        self
+    pub fn add_action(self, device_action: impl FnOnce(S) -> DeviceAction) -> Self {
+        let device_action = device_action(self.state.clone());
+        self.add_device_action(device_action)
     }
 
-    // Creates a new instance defining the DeviceKind.
     #[inline]
-    pub(crate) fn new(kind: DeviceKind) -> Self {
+    pub(crate) fn init(kind: DeviceKind, state: S) -> Self {
         Self {
             main_route: DEFAULT_MAIN_ROUTE,
             router: Router::new(),
             kind,
             routes_hazards: RoutesHazards::empty(),
+            state,
         }
     }
+
+    #[inline]
+    pub(crate) fn add_device_action(mut self, device_action: DeviceAction) -> Self {
+        self.router = self.router.merge(device_action.router);
+        self.routes_hazards.add(device_action.route_hazards);
+        self
+    }
+}
 }
