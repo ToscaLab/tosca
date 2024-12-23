c785a01277504c2dfed2ea52ae8c05edf380f465
Luni-4 <luni-4@hotmail.it>
2024-12-23 17:15:42 +0100

    library: Make an action error a passive structure
    It can be used only to receive data

--- src/actions.rs
+++ src/actions.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-use crate::strings::{MiniString, ShortString};
-
 // REMINDER:
 // 1. Parse an action response to verify whether it is an action error
 // 2. Parse an action response according to the description contained in the
@@ -20,44 +18,12 @@ pub enum ActionErrorKind {
 }
 
 /// An action error data.
-#[derive(Serialize, Deserialize)]
-pub struct ActionError {
+#[derive(Deserialize)]
+pub struct ActionError<'a> {
     /// Action error kind.
     pub kind: ActionErrorKind,
     /// Error description.
-    pub description: MiniString,
+    pub description: &'a str,
     /// Information about the error.
-    pub info: Option<ShortString>,
-}
-
-impl ActionError {
-    /// Creates a new [`ActionError`] where the description of the error is
-    /// passed as a string slice.
-    #[inline(always)]
-    pub fn from_str(kind: ActionErrorKind, description: &str) -> Self {
-        Self {
-            kind,
-            description: MiniString::new(description).unwrap_or(MiniString::empty()),
-            info: None,
-        }
-    }
-
-    /// Creates a new [`ActionError`] of kind [`ActionErrorKind::InvalidData`].
-    #[inline(always)]
-    pub fn invalid_data(description: &str) -> Self {
-        Self::from_str(ActionErrorKind::InvalidData, description)
-    }
-
-    /// Creates a new [`ActionError`] of kind [`ActionErrorKind::Internal`].
-    #[inline(always)]
-    pub fn internal(description: &str) -> Self {
-        Self::from_str(ActionErrorKind::Internal, description)
-    }
-
-    /// Adds information about the error.
-    #[inline(always)]
-    pub fn info(mut self, info: &str) -> Self {
-        self.info = Some(ShortString::new(info).unwrap_or(ShortString::empty()));
-        self
-    }
+    pub info: Option<&'a str>,
 }
