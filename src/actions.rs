b512944ec1b15136aa3226fbc93e17613a1b48e8
Luni-4 <luni-4@hotmail.it>
2025-01-13 15:53:43 +0100

    library: Improve documentation

--- src/actions.rs
+++ src/actions.rs
@@ -4,10 +4,10 @@ use serde::{Deserialize, Serialize};
 // 1. Parse an action response to verify whether it is an action error.
 // 2. If it is not an error, evaluate the respective payload.
 
-/// All possible action errors which led a device action to fail.
+/// All possible errors which might led a device action to fail.
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum ActionError {
-    /// Data needed for an action is not correct because invalid or malformed.
+    /// Action data is not correct because invalid or malformed.
     #[serde(rename = "Invalid Data")]
     InvalidData,
     /// An internal error occurred on a device during the execution of an
