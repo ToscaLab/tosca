60fa04af4caddd0b230e34d3c401beaa9f6920d4
Luni-4 <luni-4@hotmail.it>
2025-09-25 18:53:42 +0200

    misc: Remove ascot-stack crate
    This crate has never been an integral part of Ascot, but rather an
    experiment to explore the feasibility of creating a heapless version of
    the Ascot crate.

--- crates/ascot-stack/src/hazards.rs
+++
@@ -1,9 +0,0 @@
-use crate::collections::OutputSet;
-
-pub use ascot::hazards::{ALL_HAZARDS, Category, Hazard, HazardData};
-
-/// A collection of [`Hazard`]s.
-///
-/// **For alignment reasons, it accepts only a power of two
-/// as number of elements.**
-pub type Hazards<const N: usize> = OutputSet<Hazard, N>;
