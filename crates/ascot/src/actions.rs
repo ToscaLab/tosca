67d28bd3acac868954a1903fe7c92a1670e1d371
Luni-4 <luni-4@hotmail.it>
2025-09-30 15:05:16 +0200

    tosca: Rename ActionError to ErrorKind
    Removes a complicated abstraction over an error which belongs to the
    error response only.

--- crates/ascot/src/actions.rs
+++ crates/ascot/src/actions.rs
@@ -1,13 +1,1 @@
 use serde::{Deserialize, Serialize};
-
-/// All possible errors which might led a device action to fail.
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub enum ActionError {
-    /// Action data is not correct because invalid or malformed.
-    #[serde(rename = "Invalid Data")]
-    InvalidData,
-    /// An internal error occurred on a device during the execution of an
-    /// action.
-    #[serde(rename = "Internal")]
-    Internal,
-}
